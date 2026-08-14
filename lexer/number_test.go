7d3756d95c97fc4cc08535fa14136101ee9cca33
Ky Anh Vo <tommyvo0406@gmail.com>
2026-08-14 11:50:24 -0500

    add test for decimal, binary, and octal ints

--- lexer/number_test.go
+++ lexer/number_test.go
@@ -2,6 +2,55 @@ package lexer
 
 import "testing"
 
+func TestInteger(t *testing.T) {
+	type TestCase struct {
+		Label       string
+		Src         string
+		ExpectedNum Token
+	}
+
+	tcs := []TestCase{
+		{"decimal no dash", "42 ", TokenIntLit("42")},
+		{"decimal dash", "4_29_350 ", TokenIntLit("4_29_350")},
+		{"binary no dash", "0b01 ", TokenIntLit("0b01")},
+		{"binary dash", "0B_1011_1010_1101 ", TokenIntLit("0B_1011_1010_1101")},
+		{"octal no dash", "0o76770406 ", TokenIntLit("0o76770406")},
+		{"octal dash", "0O_0123_4567 ", TokenIntLit("0O_0123_4567")},
+	}
+
+	for _, tc := range tcs {
+		l := NewLexer([]rune(tc.Src))
+		token, _, err := l.getNumericToken()
+		if err != nil {
+			t.Errorf(
+				"\nTestInteger\n"+
+					"\t%s\n"+
+					"\t\terror: %s\n",
+				tc.Label,
+				err.Error(),
+			)
+		} else if token != tc.ExpectedNum {
+			t.Errorf(
+				"\nTestInteger\n"+
+					"\t%s\n"+
+					"\t\tExpected: %+v\n"+
+					"\t\tGot: %+v\n",
+				tc.Label,
+				tc.ExpectedNum,
+				token,
+			)
+		} else if l.peekNextChar() != ' ' {
+			t.Errorf(
+				"\nTestInteger\n"+
+					"\t%s\n"+
+					"\t\terror: did not advance over token, final char: %c\n",
+				tc.Label,
+				l.peekNextChar(),
+			)
+		}
+	}
+}
+
 func TestDigits(t *testing.T) {
 	type TestCase struct {
 		Src         string
