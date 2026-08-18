bbc97e13a9ad8dea2fc88158861350dbdf069d9f
Ky Anh Vo <tommyvo0406@gmail.com>
2026-08-18 02:23:33 -0500

    main, set add

--- util/set.go
+++ util/set.go
@@ -1,5 +1,7 @@
 package util
 
+import "errors"
+
 type Set[T comparable] map[T]struct{}
 
 func NewSet[T comparable](lst []T) Set[T] {
@@ -13,4 +15,70 @@ func NewSet[T comparable](lst []T) Set[T] {
 func (s Set[T]) Contains(val T) bool {
 	_, ok := s[val]
 	return ok
+}
+
+type Queue[T any] struct {
+	buf  []T
+	cap  int
+	len  int
+	head int
+	tail int
+}
+
+func NewQueue[T any](cap int) *Queue[T] {
+	return &Queue[T]{
+		buf:  make([]T, cap),
+		cap:  cap,
+		len:  0,
+		head: 0,
+	}
+}
+
+func (q *Queue[T]) At(loc int) (T, error) {
+	var val T
+	if q.len <= loc {
+		return val, errors.New("loc > q size")
+	}
+	return q.buf[(q.head+loc)%q.cap], nil
+}
+
+func (q *Queue[T]) Enqueue(val T) error {
+	if q.len == q.cap {
+		return errors.New("exceed length")
+	}
+	q.buf[(q.head+q.len)%q.cap] = val
+	q.len += 1
+	return nil
+}
+
+func (q *Queue[T]) Dequeue() (T, error) {
+	var val T
+	if q.len == 0 {
+		return val, errors.New("empty buffer")
+	}
+	val = q.buf[q.head]
+	q.head = (q.head + 1) % q.cap
+	q.len -= 1
+	return val, nil
+}
+
+func (q *Queue[T]) Push(val T) error {
+	if q.len == q.cap {
+		return errors.New("exceed length")
+	}
+	q.head = (q.head + q.cap - 1) % q.cap
+	q.buf[q.head] = val
+	q.len += 1
+	return nil
+}
+
+func (q *Queue[T]) Pop() (T, error) {
+	var val T
+	if q.len == 0 {
+		return val, errors.New("empty buffer")
+	}
+	val = q.buf[q.head]
+	q.head = (q.head + 1) % q.cap
+	q.len -= 1
+	return val, nil
 }
